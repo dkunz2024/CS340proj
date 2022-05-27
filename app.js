fabf1c84d976ba63455b51ee93f97a4007fd7b86
dkunz2024 <dkunz2024@gmail.com>
2022-05-26 17:47:41 -0700

    step 4 completed

--- app.js
+++ app.js
@@ -23,9 +23,14 @@ app.set('view engine', '.hbs');                 // Tell express to use the handl
     ROUTES
 */
 app.get('/', function(req, res)
-    {
-        res.render('index');                    // Note the call to render() and not send(). Using render() ensures the templating engine
-    });                                         // will process this file, before sending the finished HTML to the client.                                  // requesting the web site.
+    {  
+        let query1 = "SELECT * FROM customers;";               // Define our query
+
+        db.pool.query(query1, function(error, rows, fields){    // Execute the query
+
+            res.render('index', {data: rows});                  // Render the index.hbs file, and also send the renderer
+        })                                                      // an object where 'data' is equal to the 'rows' we
+    });                                                         // received back from the query                              // requesting the web site.
 
 /*
     LISTENER
