957a2d1f4e4167762d748bed611d2e9302561042
Garrett Berliner <berlineg@flip1.engr.oregonstate.edu>
2022-05-29 18:36:10 -0700

    Got step 5 working via Method 2.

--- app.js
+++ app.js
@@ -5,9 +5,13 @@
 */
 const util = require('util');
 require('util.promisify').shim();
+var mysql = require("mysql");
 
 var express = require('express');   // We are using the express library for the web server
 var app     = express();            // We need to instantiate an express object to interact with the server in our code
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
 PORT        = 8990;                 // Set a port number at the top so it's easy to change in the future
 
 // Database
@@ -16,6 +20,7 @@ var db = require('./database/db-connector')
 // Handlebars
 const { engine } = require('express-handlebars');
 var exphbs = require('express-handlebars');     // Import express-handlebars
+const { Http2ServerRequest } = require('http2');
 app.engine('.hbs', engine({extname: ".hbs"}));  // Create an instance of the handlebars engine to process templates
 app.set('view engine', '.hbs');                 // Tell express to use the handlebars engine whenever it encounters a *.hbs file.
 
@@ -31,6 +36,47 @@ app.get('/', function(req, res)
             res.render('index', {data: rows});                  // Render the index.hbs file, and also send the renderer
         })                                                      // an object where 'data' is equal to the 'rows' we
     });                                                         // received back from the query                              // requesting the web site.
+
+// app.js
+
+app.post('/add-customer-form', function(req, res){
+    // Capture the incoming data and parse it back to a JS object
+    let data = req.body;
+
+    // Capture NULL values
+    let td = parseInt(data['input-total_dropoff']);
+    if (isNaN(td))
+    {
+        td = 'NULL'
+    }
+
+    let tr = parseInt(data['input-total_recycle']);
+    if (isNaN(tr))
+    {
+        tr = 'NULL'
+    }
+
+    // Create the query and run it on the database
+    query1 = `INSERT INTO customers(customer_name, total_dropoff, total_recycle) VALUES ('${data['input-name']}', ${data['input-total-dropoff']}, ${data['input-total-recycle']})`;
+    db.pool.query(query1, function(error, rows, fields){
+
+        // Check to see if there was an error
+        if (error) {
+
+            // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+            console.log(error)
+            res.sendStatus(400);
+        }
+
+        // If there was no error, we redirect back to our root route, which automatically runs the SELECT * FROM bsg_people and
+        // presents it on the screen
+        else
+        {
+            res.redirect('/');
+        }
+    })
+})
+
 
 /*
     LISTENER
